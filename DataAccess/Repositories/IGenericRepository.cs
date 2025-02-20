7bc0a0114c8a92a17999f931fbf6f264361eff0c
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-20 08:46:41 +0700

    Add models : entites, context, appsetting ; GenericRepository

---
+++ DataAccess/Repositories/IGenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        // Synchronous methods
+        IQueryable<T> GetAllQueryable(string includeProperties = "");
+        T Get(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Delete(T entity);
+        void DeleteById(int id);
+        void Save();
+
+        // Asynchronous methods
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, string includeProperties = "");
+        Task<IEnumerable<T>> GetAllAsync(string includeProperties = "");
+        Task<T> GetByIdAsync(int id);
+        Task InsertAsync(T entity);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, string includeProperties = "");
+        Task SaveAsync();
+    }
+}
