629c8defc5ba82fc9c14dc95d75da9dae2a82dde
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-20 10:24:44 +0700

    Add login function first time : login request dto,service,controller api

---
+++ BusinessLogic/Services/Implementations/AuthSerivce.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic.DTOs.Authentication;
+using BusinessLogic.Services.Interfaces;
+using DataAccess.Models;
+using DataAccess.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.Implementations
+{
+    public class AuthSerivce : IAuthService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AuthSerivce(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<User> LoginAsync(LoginRequestDTO request)
+        {
+            var userRepository = _unitOfWork.GetRepository<User>();
+
+            var user = await userRepository.GetAsync(u =>
+                (u.Username == request.UsernameOrEmail || u.Email == request.UsernameOrEmail)
+                && u.Password == request.Password
+                && u.Status == true);
+
+            if (user == null)
+            {
+                throw new Exception("Tên đăng nhập/email hoặc mật khẩu không đúng");
+            }
+
+            return user;
+        }
+    }
+}
---
+++ ChildTrackerAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogic.DTOs.Authentication;
+using BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthChildTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public UserController(IAuthService userService)
+        {
+            _authService = userService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO request)
+        {
+            try
+            {
+                var user = await _authService.LoginAsync(request);
+
+                // Tạo response object, bỏ đi các thông tin nhạy cảm
+                var response = new
+                {
+                    user.UserId,
+                    user.Username,
+                    user.Email,
+                    user.FullName,
+                    user.Role,
+                    user.Status
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
